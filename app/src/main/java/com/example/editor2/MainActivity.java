95a7d1e17732ef40f54911d71a4b1dfa1d6b90f8
istefania <ionescu01_stefania@yahoo.ro>
2021-01-15 09:12:51 +0200

    .

--- app/src/main/java/com/example/editor2/MainActivity.java
+++ app/src/main/java/com/example/editor2/MainActivity.java
@@ -1,12 +1,5 @@
 package com.example.editor2;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.ActionBarDrawerToggle;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.drawerlayout.widget.DrawerLayout;
-
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -16,9 +9,10 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
-import android.provider.Telephony;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ExpandableListView;
 import android.widget.ImageView;
@@ -26,28 +20,36 @@ import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     ImageView imageView;
     private int PICK_IMAGE_REQUEST = 1;
 
     Bitmap bitmap;
 
+    BottomNavigationView bottomNavigationView;
+
     private ExpandableListView listView;
     private ExpandableListAdapter listAdapter;
     private List<String> listDataHeader;
     private HashMap<String,List<String>> listHash;
-
 
 
     private TextView textViewContrast;
@@ -58,9 +60,9 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     private SeekBar seekBarSaturation;
     DrawerLayout drawerLayout;
     Toolbar toolbar;
-    NavigationView navigationView;
     ActionBarDrawerToggle toggle;
     Button apply;
+
 
 
     @SuppressLint("RestrictedApi")
@@ -70,9 +72,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         setContentView(R.layout.activity_main);
         imageView = (ImageView) findViewById(R.id.image_view);
 
-        Button selectImage = findViewById(R.id.choose_image_btn);
-        Button saveImage=findViewById(R.id.save_image_btn);
-        apply=(Button)findViewById(R.id.apply_button);
+        final Button saveImage = findViewById(R.id.save_image_btn);
+        apply = (Button) findViewById(R.id.apply_button);
 
         textViewContrast=(TextView)findViewById(R.id.textView_contrast);
         seekBarContrast=(SeekBar)findViewById(R.id.seekBar_contrast);
@@ -86,8 +87,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         drawerLayout=findViewById(R.id.drawer);
         toolbar=findViewById(R.id.toolbar);
-        navigationView=findViewById(R.id.navigation_drawer);
-        navigationView.setNavigationItemSelectedListener(this);
 
         setSupportActionBar(toolbar);
         getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
@@ -96,17 +95,25 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
 
+        //get the listview
         listView = (ExpandableListView)findViewById(R.id.expandable_list);
+
+        //preparing list data
         initData();
         listAdapter= new ExpandableListAdapter(this,listDataHeader,listHash);
+
+        //setting list adapter
         listView.setAdapter(listAdapter);
+
+        bottomNavigationView=(BottomNavigationView)findViewById(R.id.bottom_menu_id);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
 
 
         seekBarContrast.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 seekBarContrast.setProgress(progress);
-                textViewContrast.setText(""+progress+"%");
+                textViewContrast.setText(""+(progress*100)/255+"%");
 
             }
 
@@ -127,7 +134,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 seekBarBrightness.setProgress(progress);
-                textViewBrightness.setText(""+progress+"%");
+                textViewBrightness.setText(""+(progress*100)/255+"%");
                 SetContrast(bitmap, progress);
                 ImageView imageView = findViewById(R.id.image_view);
                 imageView.setImageBitmap(bitmap);
@@ -167,44 +174,27 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         });
 
-        saveImage.setOnClickListener(new View.OnClickListener() {
+
+       /* saveImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                FileOutputStream outStream = null;
-                File sdCard = Environment.getExternalStorageDirectory();
-                File dir = new File(sdCard.getAbsolutePath() + "/Camera");
-                dir.mkdirs();
-                String fileName = String.format("%d.jpg", System.currentTimeMillis());
-                File outFile = new File(dir, fileName);
-                try {
-                    outStream = new FileOutputStream(outFile);
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
+
+                if (imageView.getDrawable() == null) {
+
+                    Toast.makeText(MainActivity.this, "Pick a photo", Toast.LENGTH_SHORT).show();
+
+                } else {
+                    imageView.setDrawingCacheEnabled(true);
+                    Bitmap bmap = imageView.getDrawingCache();
+                    saveImage(bmap,"000");
+
+
+                    Toast.makeText(MainActivity.this, "Saved", Toast.LENGTH_SHORT).show();
                 }
-                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outStream);
-                try {
-                    outStream.flush();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                try {
-                    outStream.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                Toast.makeText(MainActivity.this, "salvaram poza", Toast.LENGTH_SHORT).show();
-            }
+    }
 
+        });*/
 
-        });
-
-
-        selectImage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                chooseImage();
-            }
-        });
 
 
         apply.setOnClickListener(new View.OnClickListener() {
@@ -217,10 +207,10 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                     int brightness_value = seekBarBrightness.getProgress();
                     int saturation_value = seekBarSaturation.getProgress();
                     Bitmap bitmap1 = SetContrast(bitmap, contrast_value);
-                    Bitmap bitmap2 = SetBrightness(bitmap1, brightness_value);
-                    Bitmap bitmap3 = SetSaturation(bitmap2, saturation_value);
-                    ImageView imageView = findViewById(R.id.image_view);
-                    imageView.setImageBitmap(bitmap3);
+                    bitmap1 = SetBrightness(bitmap1, brightness_value);
+                    /*bitmap1 = SetSaturation(bitmap1, saturation_value);
+                    ImageView imageView = findViewById(R.id.image_view);*/
+                    imageView.setImageBitmap(bitmap1);
                     drawerLayout.closeDrawers();
                 }
                 else{
@@ -237,7 +227,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     private void initData() {
         listDataHeader = new ArrayList<>();
-        listHash = new HashMap<>();
+        listHash = new HashMap<String,List<String>>();
 
         listDataHeader.add("Rotate");
         listDataHeader.add("Flip");
@@ -249,6 +239,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         List<String> rotate= new ArrayList<>();
         rotate.add("Left");
         rotate.add("Right");
+
 
         listHash.put(listDataHeader.get(0),flip);
         listHash.put(listDataHeader.get(1),rotate);
@@ -282,17 +273,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                     e.printStackTrace();
                 }
         }
-    }
-
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-        switch (menuItem.getItemId()){
-            //case R.id.flip:
-
-            //case R.id.rotate:
-        }
-
-        return true;
     }
 
     public Bitmap SetContrast(Bitmap src, double value) {
@@ -428,6 +408,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         return bmOut;
     }
 
+
     public Bitmap RotateImage(Bitmap src, float degree) {
         // create new matrix object
         Matrix matrix = new Matrix();
@@ -435,6 +416,50 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         matrix.postRotate(degree);
         // return new bitmap rotated using matrix
         return Bitmap.createBitmap(src, 0, 0, src.getWidth(), src.getHeight(), matrix, true);
+    }
+
+    private void saveImage(Bitmap finalBitmap, String image_name) {
+
+        String root = Environment.getExternalStorageDirectory().toString();
+        File myDir = new File(root);
+        myDir.mkdirs();
+        String fname = "Image-" + image_name+ ".jpg";
+        File file = new File(myDir, fname);
+        if (file.exists()) file.delete();
+        Log.i("LOAD", root + fname);
+        try {
+            FileOutputStream out = new FileOutputStream(file);
+            finalBitmap.compress(Bitmap.CompressFormat.JPEG, 90, out);
+            out.flush();
+            out.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+        switch (menuItem.getItemId()){
+            case R.id.choose_photo_id:
+                chooseImage();
+                break;
+            case R.id.save_photo_id:
+                if (imageView.getDrawable() == null) {
+
+                    Toast.makeText(MainActivity.this, "Pick a photo", Toast.LENGTH_SHORT).show();
+
+                } else {
+                    imageView.setDrawingCacheEnabled(true);
+                    Bitmap bmap = imageView.getDrawingCache();
+                    saveImage(bmap,"000");
+                    Toast.makeText(MainActivity.this, "Saved", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case R.id.undo_id:
+                break;
+        }
+        return false;
     }
 
 }
