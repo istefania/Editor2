2628e9f37de57db066dd83864343dcf2faa50466
istefania <ionescu01_stefania@yahoo.ro>
2021-08-24 11:06:13 +0300

    Share Image

--- app/src/main/java/com/example/editor2/MainActivity.java
+++ app/src/main/java/com/example/editor2/MainActivity.java
@@ -5,8 +5,6 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
-import android.graphics.Color;
-import android.graphics.Matrix;
 import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
@@ -21,6 +19,7 @@ import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBarDrawerToggle;
@@ -29,6 +28,7 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -47,7 +47,14 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.UUID;
 
+
 public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    Rotate rotate=new Rotate();
+    Flip flip= new Flip();
+    Contrast contrast=new Contrast();
+    Brightness brightness=new Brightness();
+    Saturation saturation=new Saturation();
 
     ImageView imageView;
     private int PICK_IMAGE_REQUEST = 1;
@@ -76,14 +83,19 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
     FirebaseStorage storage;
     StorageReference storageReference;
 
+    Button imageShare;
+
     @SuppressLint("RestrictedApi")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addShareImageFragment();
+
         imageView = (ImageView) findViewById(R.id.image_view);
 
         apply = (Button) findViewById(R.id.apply_button);
+        imageShare=(Button) findViewById(R.id.fb_share_button);
 
         textViewContrast=(TextView)findViewById(R.id.textView_contrast);
         seekBarContrast=(SeekBar)findViewById(R.id.seekBar_contrast);
@@ -126,13 +138,13 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
 
                    if(childPosition==0)
                    {
-                       bitmap=FlipImage(bitmap,1);
+                       bitmap=flip.FlipImage(bitmap,1);
                        imageView.setImageBitmap(bitmap);
                        drawerLayout.closeDrawers();
                    }
                    else
                    {
-                       bitmap=FlipImage(bitmap,2);
+                       bitmap=flip.FlipImage(bitmap,2);
                        imageView.setImageBitmap(bitmap);
                        drawerLayout.closeDrawers();
                    }
@@ -141,12 +153,12 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
                if(groupPosition==1){
                    if(childPosition==0)
                    {
-                       bitmap=RotateImage(bitmap,90);
+                       bitmap=rotate.RotateImage(bitmap,90);
                        imageView.setImageBitmap(bitmap);
                        drawerLayout.closeDrawers();
                    }
                    else{
-                       bitmap= RotateImage(bitmap,-90);
+                       bitmap= rotate.RotateImage(bitmap,-90);
                        imageView.setImageBitmap(bitmap);
                        drawerLayout.closeDrawers();
                    }
@@ -220,10 +232,9 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 seekBarBrightness.setProgress(progress);
                 textViewBrightness.setText(""+(progress*100)/255+"%");
-                SetContrast(bitmap, progress);
+                brightness.SetBrightness(bitmap, progress);
                 ImageView imageView = findViewById(R.id.image_view);
                 imageView.setImageBitmap(bitmap);
-
 
             }
 
@@ -269,10 +280,10 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
                     int contrast_value = seekBarContrast.getProgress();
                     int brightness_value = seekBarBrightness.getProgress();
                     int saturation_value = seekBarSaturation.getProgress();
-                    Bitmap bitmap1 = SetContrast(bitmap, contrast_value);
-                    bitmap1 = SetBrightness(bitmap1, brightness_value);
-                    /*bitmap1 = SetSaturation(bitmap1, saturation_value);
-                    ImageView imageView = findViewById(R.id.image_view);*/
+                    Bitmap bitmap1 = contrast.SetContrast(bitmap, contrast_value);
+                    bitmap1 = brightness.SetBrightness(bitmap1, brightness_value);
+                    bitmap1 = saturation.SetSaturation(bitmap1, saturation_value);
+                    ImageView imageView = findViewById(R.id.image_view);
                     imageView.setImageBitmap(bitmap1);
                     drawerLayout.closeDrawers();
                     bitmapArray.add(bitmap1);
@@ -310,41 +321,12 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
 
     }
 
-
     private void chooseImage() {
         Intent intent = new Intent();
         intent.setType("image/*");
         intent.setAction(Intent.ACTION_GET_CONTENT);
         startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
 
-    }
-
-    private void saveInFirebase(){
-        if (imageView.getDrawable()!=null) {
-
-            StorageReference reference=storageReference.child("Picture/"+ UUID.randomUUID().toString());
-
-            try{
-            reference.putFile(imgUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                @Override
-                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                    Toast.makeText(MainActivity.this, "Saved successfully",Toast.LENGTH_SHORT).show();
-                }
-            }).addOnFailureListener(new OnFailureListener() {
-                @Override
-                public void onFailure(@NonNull Exception e) {
-                    Toast.makeText(MainActivity.this, "Upload failed",Toast.LENGTH_SHORT).show();
-                }
-            }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
-                @Override
-                public void onProgress(@NonNull UploadTask.TaskSnapshot taskSnapshot) {
-
-                }
-            });
-            }catch (Exception e){
-                e.printStackTrace();
-            }
-        }
     }
 
 
@@ -369,168 +351,39 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         }
     }
 
-    public Bitmap SetContrast(Bitmap src, double value) {
-        // src image size
-        int width = src.getWidth();
-        int height = src.getHeight();
-        // create output bitmap with original size
-        Bitmap bmOut = Bitmap.createBitmap(width, height, src.getConfig());
-        // color information
-        int A, R, G, B;
-        int pixel;
-        // get contrast value
-        double contrast = Math.pow((100 + value) / 100, 2);
+    private void addShareImageFragment() {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.facebook_share_button, new ShareImageFragment());
+        fragmentTransaction.commit();
+    }
 
-        // scan through all pixels
-        for (int x = 0; x < width; ++x) {
-            for (int y = 0; y < height; ++y) {
-                // get pixel color
-                pixel = src.getPixel(x, y);
-                A = Color.alpha(pixel);
-                // apply filter contrast for every channel R, G, B
-                R = Color.red(pixel);
-                R = (int) (((((R / 255.0) - 0.5) * contrast) + 0.5) * 255.0);
-                if (R < 0) {
-                    R = 0;
-                } else if (R > 255) {
-                    R = 255;
-                }
 
-                G = Color.green(pixel);
-                G = (int) (((((G / 255.0) - 0.5) * contrast) + 0.5) * 255.0);
-                if (G < 0) {
-                    G = 0;
-                } else if (G > 255) {
-                    G = 255;
-                }
+    void saveInFirebase(){
+        if (imageView.getDrawable()!=null) {
 
-                B = Color.blue(pixel);
-                B = (int) (((((B / 255.0) - 0.5) * contrast) + 0.5) * 255.0);
-                if (B < 0) {
-                    B = 0;
-                } else if (B > 255) {
-                    B = 255;
-                }
+            StorageReference reference=storageReference.child("Picture/"+ UUID.randomUUID().toString());
 
-                // set new pixel color to output bitmap
-                bmOut.setPixel(x, y, Color.argb(A, R, G, B));
+            try{
+                reference.putFile(imgUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        Toast.makeText(MainActivity.this, "Saved successfully",Toast.LENGTH_SHORT).show();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(MainActivity.this, "Upload failed",Toast.LENGTH_SHORT).show();
+                    }
+                }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onProgress(@NonNull UploadTask.TaskSnapshot taskSnapshot) {
+
+                    }
+                });
+            }catch (Exception e){
+                e.printStackTrace();
             }
         }
-
-        // return final image
-        return bmOut;
-    }
-    public Bitmap SetBrightness(Bitmap src, int value) {
-
-        // original image size
-        int width = src.getWidth();
-        int height = src.getHeight();
-        // create output bitmap
-        Bitmap bmOut = Bitmap.createBitmap(width, height, src.getConfig());
-        // color information
-        int A, R, G, B;
-        int pixel;
-
-        // scan through all pixels
-        for (int x = 0; x < width; ++x) {
-            for (int y = 0; y < height; ++y) {
-                // get pixel color
-                pixel = src.getPixel(x, y);
-                A = Color.alpha(pixel);
-                R = Color.red(pixel);
-                G = Color.green(pixel);
-                B = Color.blue(pixel);
-
-                // increase/decrease each channel
-                R += value;
-                if (R > 255) {
-                    R = 255;
-                } else if (R < 0) {
-                    R = 0;
-                }
-
-                G += value;
-                if (G > 255) {
-                    G = 255;
-                } else if (G < 0) {
-                    G = 0;
-                }
-
-                B += value;
-                if (B > 255) {
-                    B = 255;
-                } else if (B < 0) {
-                    B = 0;
-                }
-
-                // apply new pixel color to output bitmap
-                bmOut.setPixel(x, y, Color.argb(A, R, G, B));
-            }
-        }
-
-        // return final image
-        return bmOut;
-    }
-    @SuppressLint("Range")
-    public Bitmap SetSaturation(Bitmap source, int level) {
-        // get original image size
-        int width = source.getWidth();
-        int height = source.getHeight();
-        int[] pixels = new int[width * height];
-        float[] HSV = new float[100];
-        // get pixel array from source image
-        source.getPixels(pixels, 0, width, 0, 0, width, height);
-
-        int index = 0;
-        // iteration through all pixels
-        for (int y = 0; y < height; ++y) {
-            for (int x = 0; x < width; ++x) {
-                // get current index in 2D-matrix
-                index = y * width + x;
-                // convert to HSV
-                Color.colorToHSV(pixels[index], HSV);
-                // increase Saturation level
-                HSV[1] *= level;
-                HSV[1] = (float) Math.max(0.0, Math.min(HSV[1], 1.0));
-                // take color back
-                pixels[index] = Color.HSVToColor(HSV);
-            }
-        }
-        // output bitmap
-        Bitmap bmOut = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
-        bmOut.setPixels(pixels, 0, width, 0, 0, width, height);
-        return bmOut;
-    }
-
-    public Bitmap FlipImage(Bitmap src, int type) {
-        // create new matrix for transformation
-        Matrix matrix = new Matrix();
-        // if vertical
-        if (type == 1) {
-            // y = y * -1
-            matrix.preScale(1.0f, -1.0f);
-        }
-        // if horizonal
-        else if (type == 2) {
-            // x = x * -1
-            matrix.preScale(-1.0f, 1.0f);
-            // unknown type
-        }else {
-            return null;
-        }
-
-        // return transformed image
-        Bitmap flippedBitmap = Bitmap.createBitmap(src, 0, 0, src.getWidth(), src.getHeight(), matrix, true);
-        return flippedBitmap;
-    }
-
-    public Bitmap RotateImage(Bitmap src, float degree) {
-        // create new matrix object
-        Matrix matrix = new Matrix();
-        // setup rotation degree
-        matrix.postRotate(degree);
-        // return new bitmap rotated using matrix
-        return Bitmap.createBitmap(src, 0, 0, src.getWidth(), src.getHeight(), matrix, true);
     }
 
     @Override
